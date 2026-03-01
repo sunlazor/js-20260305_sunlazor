d31a031174ce945bf899b8b18f6d10eea9caec6a
javascriptru <67475495+jsru-1@users.noreply.github.com>
2026-03-01 17:02:02 +0300

    update workspace

--- jest-setup-files-after-env.js
+++
@@ -1,9 +0,0 @@
-import '@testing-library/jest-dom';
-import fetchMock from 'jest-fetch-mock';
-
-fetchMock.enableMocks();
-
-process.on('unhandledRejection', (reason, promise) => {
-  // console.log('reason: ', reason, 'promise: ', promise);
-  /* Заглушка в node для window.addEventListener('unhandledrejection') */
-});
