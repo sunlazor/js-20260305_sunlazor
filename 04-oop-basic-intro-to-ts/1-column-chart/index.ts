7dd3dd0972dd8480e64a6fa646f70cd05d54c7a6
sunlazor <1979844+sunlazor@users.noreply.github.com>
2026-03-19 21:46:58 +0100

    task 4-1; Implement interface Options

--- 04-oop-basic-intro-to-ts/1-column-chart/index.ts
+++ 04-oop-basic-intro-to-ts/1-column-chart/index.ts
@@ -1,11 +1,20 @@
-import { createElement } from "../../../shared/utils/create-element";
+import { createElement } from "../../shared/utils/create-element";
 
 interface Options {
-
+  // массив чисел (данные графика)
+  data: number[];
+  // заголовок (например, "orders")
+  label: string;
+  // основное числовое значение (например, "344")
+  value: number;
+  // ссылка "View all" (если передана)
+  link: string;
+  // функция для форматирования значения value (например, добавление знака валюты $)
+  formatHeading: Function;
 }
 
 export default class ColumnChart {
   constructor({ }: Options = {}) {
-    
+
   }
 }
