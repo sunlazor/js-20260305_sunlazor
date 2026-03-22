045df7b855aa039abcaf1b6def145246d3eeb123
sunlazor <1979844+sunlazor@users.noreply.github.com>
2026-03-22 20:45:46 +0100

    task 3-2; Implement NotificationMessage component

--- 05-dom-document-loading/1-notification/index.ts
+++ 05-dom-document-loading/1-notification/index.ts
@@ -1,9 +1,55 @@
-import { createElement } from "../../shared/utils/create-element";
+import {createElement} from "../../shared/utils/create-element";
 
-interface Options { }
+interface Options {
+  duration?: number;
+  type?: 'success' | 'error';
+}
 
 export default class NotificationMessage {
+  private static element: HTMLElement;
 
-  constructor(message: string, { }: Options = {}) {
+  constructor(private message: string, private options: Options = {
+    duration: 2000,
+    type: 'success',
+  }) {
+    if (NotificationMessage.element) {
+      this.remove();
+    }
+    NotificationMessage.element = this.makeNotificationTemplate();
+  }
+
+  public get element() {
+    return NotificationMessage.element
+  }
+
+  public show(target: HTMLElement | undefined) {
+    target ? target.append(NotificationMessage.element) : document.body.append(NotificationMessage.element);
+    setTimeout(this.remove, this.options.duration);
+  }
+
+  public remove() {
+    NotificationMessage.element.remove();
+  }
+
+  public destroy() {
+    NotificationMessage.element.remove();
+  }
+
+  private makeNotificationTemplate() {
+    const duration = typeof this.options?.duration === 'number'
+      ? this.options?.duration + 'ms'
+      : this.options?.duration
+    ;
+    return createElement(`
+      <div class="notification ${this.options?.type}" style="--value:${duration}">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">${this.options?.type}</div>
+          <div class="notification-body">
+            ${this.message}
+          </div>
+        </div>
+      </div>
+    `);
   }
 }
