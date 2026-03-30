82e6e22057de5f76cc4982700499f48f1bc1a256
sunlazor <1979844+sunlazor@users.noreply.github.com>
2026-03-30 16:23:17 +0200

    task 5-2; Make code little prettier

--- 05-dom-document-loading/2-sortable-table-v1/index.ts
+++ 05-dom-document-loading/2-sortable-table-v1/index.ts
@@ -23,42 +23,36 @@ export default class SortableTable {
   // Сортировка чисел — через числовое сравнение.
   // Если колонка не найдена или не сортируемая — ничего не делаем.
   public sort(field: string, order: SortOrder = 'asc') {
-    console.log('sort triggered');
-    console.log('headers: ', this.headersConfig);
-    console.log('field: ', field);
-    const fieldInHeader = this.headersConfig.filter((header) => header.id === field );
-    console.log('fieldInHeader: ', fieldInHeader);
+    const fieldInHeader
+      = this.headersConfig.filter((header) => header.id === field );
     const isFieldNotFound = fieldInHeader.length === 0;
-    console.log('isFieldNotFound: ', isFieldNotFound);
     const isFieldNotSortable = fieldInHeader[0]?.sortable !== true;
-    console.log('isFieldNotSortable: ', isFieldNotSortable);
     if (isFieldNotFound || isFieldNotSortable) {
-      console.log(' sort is dead');
       return;
     }
 
+    this.changeColumnsDataOrder(field, order);
 
-    console.log('before collCells');
-    const columnsCells
-      // = this.element?.querySelectorAll<HTMLElement>('.sortable-table__cell[data-id]') ?? [];
-      = this.element?.querySelectorAll<HTMLElement>('div[data-element="header"] .sortable-table__cell') ?? [];
-    // const isSortingExists = [...columns].some(column => column.dataset.order);
-    console.log('column cells: ', columnsCells);
-    if (columnsCells.length > 0) {
-      columnsCells.forEach((columnCell: HTMLElement) => {
-        if (columnCell.dataset.id !== field) {
-          columnCell.dataset.order = '';
-        } else {
-          columnCell.dataset.order = `${order}`;
-        }
-      })
+    const sortType = fieldInHeader[0]?.sortType ? fieldInHeader[0].sortType : null;
+    if (sortType) {
+      this.sortData(sortType, field, order);
     }
 
+    this.rewriteBodyData();
+  }
 
+  private rewriteBodyData() {
+    const bodyDiv
+      = this.element?.querySelector<HTMLElement>('div[data-element="body"]');
+    if (bodyDiv) {
+      bodyDiv.innerHTML = '';
+      this.makeTableBodyData(bodyDiv);
+    }
+  }
 
-    console.log('data before sorting: ', this.data);
+  private sortData(sortType: 'string' | 'number', field: string, order: SortOrder) {
     this.data.sort((a, b) => {
-      if (fieldInHeader[0]?.sortType === 'string') {
+      if (sortType === 'string') {
         let compareResult = a[field].toString().localeCompare(
           b[field].toString(), ['ru', 'en'], {'caseFirst': 'upper', 'sensitivity': 'variant'}
         );
@@ -67,7 +61,7 @@ export default class SortableTable {
         return direction * compareResult;
       }
 
-      if (fieldInHeader[0]?.sortType === 'number') {
+      if (sortType === 'number') {
         let compareResult =
           parseFloat(typeof a[field] === 'string' ? a[field] : a[field].toString())
           - parseFloat(typeof b[field] === 'string' ? b[field] : b[field].toString())
@@ -79,13 +73,19 @@ export default class SortableTable {
 
       return 0;
     });
-    console.log('data after sorting: ', this.data);
+  }
 
-    const bodyDiv
-      = this.element?.querySelector<HTMLElement>('div[data-element="body"]');
-    if (bodyDiv) {
-      bodyDiv.innerHTML = '';
-      this.makeTableBodyData(bodyDiv);
+  private changeColumnsDataOrder(field: string, order: SortOrder) {
+    const columnsCells
+      = this.element?.querySelectorAll<HTMLElement>('div[data-element="header"] .sortable-table__cell') ?? [];
+    if (columnsCells.length > 0) {
+      columnsCells.forEach((columnCell: HTMLElement) => {
+        if (columnCell.dataset.id !== field) {
+          columnCell.dataset.order = '';
+        } else {
+          columnCell.dataset.order = `${order}`;
+        }
+      })
     }
   }
 
@@ -121,6 +121,7 @@ export default class SortableTable {
     let body = createElement(`
       <div data-element="body" class="sortable-table__body"></div>
     `);
+
     this.makeTableBodyData(body);
 
     return body;
@@ -153,10 +154,5 @@ export default class SortableTable {
     this.remove();
     this.headersConfig = [];
     this.data = [];
-  }
-
-  private render() {
-    this.remove();
-    this.element = this.makeTableTemplate();
   }
 }
