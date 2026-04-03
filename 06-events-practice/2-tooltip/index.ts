7432a2d4c134564f23cdbbcf35b5eebcef43319a
sunlazor <1979844+sunlazor@users.noreply.github.com>
2026-04-03 21:36:26 +0200

    task 6-2; Add tooltip template

--- 06-events-practice/2-tooltip/index.ts
+++ 06-events-practice/2-tooltip/index.ts
@@ -1,3 +1,15 @@
 export default class Tooltip {
+  private tooltip: HTMLElement;
 
+  constructor() {
+    this.tooltip = this.makeTooltipTemplate();
+  }
+
+  public get element() {
+    return this.tooltip;
+  }
+
+  private makeTooltipTemplate() {
+    return document.createElement('<div class="tooltip"></div>');
+  }
 }
