7642b9b6857f29e2ebf52022335c24283581346c
sunlazor <1979844+sunlazor@users.noreply.github.com>
2026-04-05 22:14:07 +0200

    task 6-2; Add partial implementation

--- 06-events-practice/2-tooltip/index.ts
+++ 06-events-practice/2-tooltip/index.ts
@@ -1,15 +1,50 @@
+import {createElement} from "../../shared/utils/create-element";
+
 export default class Tooltip {
-  private tooltip: HTMLElement;
+  private static tooltip: HTMLElement | null = null;
 
   constructor() {
-    this.tooltip = this.makeTooltipTemplate();
+    if (!Tooltip.tooltip) {
+      Tooltip.tooltip = this.makeTooltipTemplate();
+    }
   }
 
   public get element() {
-    return this.tooltip;
+    if (!Tooltip.tooltip) {
+      Tooltip.tooltip = this.makeTooltipTemplate();
+    }
+
+    return Tooltip.tooltip;
+  }
+
+  public render(html: string | null) {
+    this.element.textContent = html;
+    document.appendChild(this.element);
+  }
+
+  public initialize() {
+    document.addEventListener('pointerover', (event) => {
+      const tooltipTarget = event.target as HTMLElement;
+      if (tooltipTarget.dataset.tooltip) {
+        this.element.textContent = tooltipTarget.dataset.tooltip;
+        tooltipTarget.appendChild(this.element);
+      }
+    });
+    document.addEventListener('pointerout', (event) => {
+      this.destroy();
+
+      // const tooltipTarget = event.target as HTMLElement;
+      // if (tooltipTarget.dataset.tooltip) {
+      //   this.tooltip.textContent = tooltipTarget.dataset.tooltip;
+      // }
+    });
+  }
+
+  public destroy() {
+    this.element.remove();
   }
 
   private makeTooltipTemplate() {
-    return document.createElement('<div class="tooltip"></div>');
+    return createElement('<div class="tooltip"></div>');
   }
 }
