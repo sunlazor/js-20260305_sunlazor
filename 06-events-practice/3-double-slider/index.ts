5cdda7ae9340126d464d2ecad7fa33d304a6cd28
sunlazor <1979844+sunlazor@users.noreply.github.com>
2026-04-15 21:49:41 +0200

    task 6-3; Clean comments and refactor listeners

--- 06-events-practice/3-double-slider/index.ts
+++ 06-events-practice/3-double-slider/index.ts
@@ -32,8 +32,7 @@ export default class DoubleSlider {
     this.min = sliderConf?.min || 0;
     this.max = sliderConf?.max || this.min + 100;
     this.formatValue = sliderConf?.formatValue ?? function(value: number) { return value.toString() };
-    // this.selected = sliderConf?.selected ?? { from: this.max * 0.25, to: this.max * 0.75};
-    this.selected = sliderConf?.selected ?? { from: 0, to: this.max * 0.75};
+    this.selected = sliderConf?.selected ?? { from: this.max * 0.25, to: this.max * 0.75};
 
     this.element = this.makeSliderTemplate();
     this.leftThumb = <HTMLSpanElement>this.element.querySelector('.range-slider__thumb-left');
@@ -80,13 +79,7 @@ export default class DoubleSlider {
   }
 
   private addRightThumbEvents(rightThumb: HTMLElement) {
-    // const leftLimit = this.leftThumb.offsetLeft + this.leftThumb.getBoundingClientRect().width;
-      // = this.element?.querySelector('.range-slider__thumb-left')?.getBoundingClientRect()?.left
-      // ?? this.element?.querySelector('.range-slider__inner')?.getBoundingClientRect()?.left
-      // ?? 0
-    // ;
     const sliderLeft= this.innerSlider.getBoundingClientRect().left;
-    const sliderRight= this.rightThumb.getBoundingClientRect().right;
     const leftLimit = this.leftThumb.offsetLeft + this.leftThumb.getBoundingClientRect().width;
     const rigthLimit = this.innerSlider.getBoundingClientRect().width;
 
@@ -99,52 +92,19 @@ export default class DoubleSlider {
       } else {
         rightThumb.style.left = inSliderCord + 'px';
       }
-      console.log(event.clientX, leftLimit, sliderRight, rightThumb.style.left);
     };
 
     document.addEventListener('pointermove', this.pointermoveEvent);
 
-    this.pointerupEvent = () => {
-      if (this.pointerdownEvent) {
-        document.removeEventListener('pointerdown', this.pointerdownEvent);
-      }
-      if (this.pointermoveEvent) {
-        document.removeEventListener('pointermove', this.pointermoveEvent);
-      }
-      if (this.pointerupEvent) {
-        document.removeEventListener('pointerup', this.pointerupEvent);
-      }
-    };
-
-    document.addEventListener('pointerup', this.pointerupEvent);
+    this.addThumbsPointerListeners();
   }
 
   private addLeftThumbEvents(leftThumb: HTMLElement) {
     const sliderLeft= this.innerSlider.getBoundingClientRect().left;
-    // const sliderLeft
-    //   = this.innerSlider.offsetLeft;
-    // console.log('this.innerSlider.offsetLeft: ', sliderLeft);
-    // console.log('this.leftThumb.offsetLeft: ', this.leftThumb.offsetLeft);
-
     const rightLimit = this.rightThumb.offsetLeft;
-      // = this.element.querySelector('.range-slider__thumb-right')?.getBoundingClientRect().left ?? sliderLeft;
-      // = parseInt(this.rightThumb.style.left) ?? sliderLeft;
-    // console.log('this.rightThumb.style.left: ', this.rightThumb.style.left);
-    // const leftThumb
-    //   = this.element?.querySelector('.range-slider__thumb-left')?.getBoundingClientRect()?.left
-    //   // ?? this.element?.querySelector('.range-slider__inner')?.getBoundingClientRect()?.left
-    //   ?? 0
-    ;
-
-    // console.log(leftLimit, leftThumb);
 
     this.pointermoveEvent = (event: PointerEvent) => {
       const inSliderCord = event.clientX - sliderLeft;
-
-      // console.log('inSliderCord: ', inSliderCord);
-      // console.log('sliderLeft: ', sliderLeft);
-      // console.log('rightLimit: ', rightLimit);
-
       if (inSliderCord < 0) {
         leftThumb.style.left = '0px';
       } else if (inSliderCord > rightLimit) {
@@ -152,11 +112,14 @@ export default class DoubleSlider {
       } else {
         leftThumb.style.left = inSliderCord + 'px';
       }
-      // console.log('leftThumb.style.left: ', leftThumb.style.left);
     };
 
     document.addEventListener('pointermove', this.pointermoveEvent);
 
+    this.addThumbsPointerListeners();
+  }
+
+  private addThumbsPointerListeners() {
     this.pointerupEvent = () => {
       if (this.pointerdownEvent) {
         document.removeEventListener('pointerdown', this.pointerdownEvent);
